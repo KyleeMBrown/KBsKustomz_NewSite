29d25a6842fe95cebddf49ecf64add72ecdfb1da
Kylee <kyleebrown.work@gmail.com>
2025-12-20 10:15:19 -0600

    updated thte postcss file tailwind works now!..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "../components/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./globals.css"
+import Navbar from "../components/Navbar"
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar></Navbar>
+    
+    <html lang="en" >
+      <body className="bg-black">
+        <Navbar/>
         {children}
       </body>
     </html>
