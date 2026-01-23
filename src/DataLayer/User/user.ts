843496938c3862860e9c9f42d45a5a064f60063b
Kylee <kyleebrown.work@gmail.com>
2026-01-23 14:01:38 -0600

    updated Datalayer user.ts

--- src/DataLayer/User/user.ts
+++ src/DataLayer/User/user.ts
@@ -5,26 +5,6 @@
  *
  */
 
-import { User } from "@/Lib/types/Types";
-
-/**
- * @name createUser
- * @description - Function that retrieves the current user object via API fetch
- * @async
- */
-
-export const handleCreateUser = async (userObject:User) => {
-  // send a request to create a user
-  const response = await fetch(`/api/user`, {
-    method: "POST",
-    body: JSON.stringify(userObject),
-  });
-
-  // get the response as json
-  const data = await response.json();
-
-  return data
-}
 /**
  * @name getUser
  * @description - Function that retrieves the current user object via API fetch
